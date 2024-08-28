dfcab7de01dd486f07edf3afcfd25d614fffe47d
swaraj kumar <2021itb022.swaraj@students.iiests.ac.in>
2024-08-28 14:17:20 +0530

    vercel update

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -23,7 +23,7 @@ function Signin(){
     async function handlelogin (e) {
         e.prevent.default();
         console.log(username, password)
-            const response = await axios.post(`${API_URL}/api/v1/user/signin`, {
+            const response = await axios.post(`${API_URL}/user/signin`, {
                 username: username,
                 password: password,
             },
