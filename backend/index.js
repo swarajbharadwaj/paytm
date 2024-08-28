b85dcc6e299c233ffdc2eca648edf60d5bc54723
swaraj kumar <2021itb022.swaraj@students.iiests.ac.in>
2024-08-28 11:06:20 +0530

    vercel update

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,15 @@ const cors = require('cors');
 app.use(express.json());
 app.use(
        cors({
-         origin: "https://pay-sphere-your-digital-vault.vercel.app/",
+         origin: "https://pay-sphere-your-digital-vault.vercel.app",
          credentials: true,
        })
      );
+     
 app.use("/api/v1", router);
 
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
